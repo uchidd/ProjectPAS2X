39020200f5d17c2439bac9cd7e1097d8d52b6d20
yteixna <uchidd.email@gmail.com>
2019-04-13 19:48:58 +0700

    Splash Screen Intent Explicit

---
+++ app/src/main/java/id/uchidd/projectpas2xaplikasionlineshopelektronik/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package id.uchidd.projectpas2xaplikasionlineshopelektronik;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
+        hideNavigationBar();
+    }
+
+
+
+    public void hideNavigationBar() {
+
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+    }
+}
